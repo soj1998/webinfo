1876252d20d096a150c0978986b49c46f5ebe084
soj1998 <736495034@qq.com>
2018-06-16 15:01:42 +0800

    16

--- src/wl/test/MainTest.java
+++ src/wl/test/MainTest.java
@@ -6,25 +6,33 @@ import java.io.IOException;
 
 
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 
 public class MainTest {
 
 	public static void main(String[] args) {
-		Document doc;
-		try {
-			String url="http://www.chinatax.gov.cn/n810341/n810780/c3498512/content.html";
-			doc = Jsoup.connect("http://www.chinatax.gov.cn/n810341/n810780/c3498512/content.html").get();
-			System.out.println(doc.html());
-			Document doc2=Jsoup.parse(new URL(url), 5000);
-			System.out.println(doc2.html());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		WebDriver webDriver = new FirefoxDriver(); //创建火狐驱动（谷歌IE需下载驱动程序并添加浏览器插件，还有注意版本对应，比较麻烦，请百度版本对应）
+        
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
+        webDriver.get("http://www.chinatax.gov.cn/n810341/n810755/c3518520/content.html");
+        
+        WebElement passLoginEle= webDriver.findElement(By.xpath("//a[@class='forget-pwd J_Quick2Static' and @target='_blank' and @href='']")); //密码登录
+        
+        passLoginEle.click(); //显示账号密码表单域（模仿点击事件，将隐藏视图变为可见）
+        
+        
+        webDriver.findElement(By.id("J_SubmitStatic")).click(); //点击登录按钮  
+        webDriver.switchTo().defaultContent();
+
 	}
 	
 
